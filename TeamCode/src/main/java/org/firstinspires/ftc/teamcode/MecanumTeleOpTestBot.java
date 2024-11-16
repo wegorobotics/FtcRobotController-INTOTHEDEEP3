d4f07fa8e8a179b58695c9e8d2f78e6d40d8d6e2
wegorobotics <wegorobotics@d94.org>
2024-11-16 10:38:11 -0600

    first attempt at making the P (error) in PID, to hopefully correct any stray movement in the linear slide...

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOpTestBot.java
@@ -0,0 +1,140 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp (name= "MecanumTeleOpTestBot")
+//@Disabled
+public class MecanumTeleOpTestBot extends OpMode {
+    // motors
+    DcMotor fl_Wheel;
+    DcMotor bl_Wheel;
+    DcMotor fr_Wheel;
+    DcMotor br_Wheel;
+    //private Limelight3A limelight;
+    Servo arm_servo;
+
+
+    //DcMotor pumpkin_smasher;
+    //Servo candy_grabber;
+
+
+    @Override
+    public void init() {
+        fl_Wheel = hardwareMap.get(DcMotor.class, "fl_motor");
+        bl_Wheel = hardwareMap.get(DcMotor.class, "bl_motor");
+        fr_Wheel = hardwareMap.get(DcMotor.class, "fr_motor");
+        br_Wheel = hardwareMap.get(DcMotor.class, "br_motor");
+        //limelight = hardwareMap.get(Limelight3A.class, "Limelight 3A");
+        arm_servo = hardwareMap.get(Servo.class, "arm");
+
+        //pumpkin_smasher = hardwareMap.get(DcMotor.class, "pumpkin_motor");
+        //candy_grabber = hardwareMap.get(Servo.class, "candy");
+
+        //pumpkin_smasher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+
+
+        fr_Wheel.setDirection(DcMotor.Direction.FORWARD);
+        fl_Wheel.setDirection(DcMotor.Direction.REVERSE);
+        br_Wheel.setDirection(DcMotor.Direction.FORWARD);
+        bl_Wheel.setDirection(DcMotor.Direction.REVERSE);
+
+        fr_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fl_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.setMsTransmissionInterval(11);
+        //limelight.pipelineSwitch(0);
+        //limelight.start();
+    }
+
+    public void loop() {
+        //testing a change for github
+        // encoders
+        double fl_Position = fl_Wheel.getCurrentPosition();
+        double bl_Position = bl_Wheel.getCurrentPosition();
+        double fr_Position = fr_Wheel.getCurrentPosition();
+        double br_Position = br_Wheel.getCurrentPosition();
+        double arm_Position = arm_servo.getPosition();
+
+        telemetry.addData("Front Left Wheel Pos", fl_Position);
+        telemetry.addData("Back Left Wheel Pos", bl_Position);
+        telemetry.addData("Front Right Wheel Pos", fr_Position);
+        telemetry.addData("Back Right Wheel Pos", br_Position);
+
+        /*
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            telemetry.addData("April Tag X Pos", result.getTx());
+            telemetry.addData("April Tag Y Pos", result.getTy());
+            telemetry.addData("April Tag Area %", result.getTa());
+            telemetry.addData("Limelight Pipeline", result.getPipelineIndex());
+        }
+        */
+
+        //pipeline 0 = chamber side
+        //pipeline 1 = basket side
+        //pipeline 2 = observation side
+
+        //double candy_bucket = candy_grabber.getPosition();
+        //telemetry.addData("Candy Bucket Pos", candy_bucket);
+
+        telemetry.update();
+
+        // wheel movement
+        double left_x = gamepad1.left_stick_x;
+        double left_y = gamepad1.left_stick_y;
+        double joystick_turn = gamepad1.right_stick_x;
+        double joystick_direction = Math.atan2(left_y, left_x);
+        double joystick_magnitude = Math.sqrt((left_x * left_x) + (left_y * left_y));
+
+        // setting power of wheels based on joystick data
+        /*
+        fr_Wheel.setPower((-1 * Math.sin(joystick_direction + (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
+        fl_Wheel.setPower((Math.sin(joystick_direction + (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
+        br_Wheel.setPower((-1 * Math.sin(joystick_direction - (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
+        bl_Wheel.setPower((Math.sin(joystick_direction - (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
+         */
+
+        fr_Wheel.setPower((Math.sin(joystick_direction + (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
+        fl_Wheel.setPower((-1 * Math.sin(joystick_direction - (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
+        br_Wheel.setPower((Math.sin(joystick_direction - (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
+        bl_Wheel.setPower((-1 * Math.sin(joystick_direction + (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
+
+        fr_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fl_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        /*
+        // spoopy stuff
+        final double candy_racing = .01;
+
+        pumpkin_smasher.setPower((gamepad1.right_trigger - gamepad1.left_trigger) / 6);
+        if (gamepad1.x) {
+            //candy_bucket += candy_racing;
+            candy_grabber.setPosition(0.4);
+        } else if (gamepad1.y) {
+            //candy_bucket -= candy_racing;
+            candy_grabber.setPosition(0.6);
+        } else if (!gamepad1.x && !gamepad1.y) {
+            candy_grabber.setPosition(0.5);
+        }
+        */
+
+    }
+}
+
+
+
+//bl_motor is actually br_motor
+//fl_motor is actually fr_motor
+//br_motor is actually fl_motor
+//fr_motor is actually bl_motor
+//bleh
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOpTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOpTesting.java
@@ -1,14 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.motors.RevRoboticsCoreHexMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 
 @TeleOp (name= "MecanumTeleOpTesting")
 //@Disabled
@@ -18,13 +15,14 @@ public class MecanumTeleOpTesting extends OpMode {
     DcMotor bl_Wheel;
     DcMotor fr_Wheel;
     DcMotor br_Wheel;
+    DcMotor placing_slide;
+    DcMotor climbing_slide;
     //private Limelight3A limelight;
     Servo arm_servo;
+    //TouchSensor magnetic_limit;
+    Servo claw_servo;
 
-
-    //DcMotor pumpkin_smasher;
-    //Servo candy_grabber;
-
+    private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void init() {
@@ -32,31 +30,38 @@ public class MecanumTeleOpTesting extends OpMode {
         bl_Wheel = hardwareMap.get(DcMotor.class, "bl_motor");
         fr_Wheel = hardwareMap.get(DcMotor.class, "fr_motor");
         br_Wheel = hardwareMap.get(DcMotor.class, "br_motor");
+        placing_slide = hardwareMap.get(DcMotor.class, "placing_motor");
+        climbing_slide = hardwareMap.get(DcMotor.class, "climbing_motor");
         //limelight = hardwareMap.get(Limelight3A.class, "Limelight 3A");
         arm_servo = hardwareMap.get(Servo.class, "arm");
+        //magnetic_limit = hardwareMap.get(TouchSensor.class, "magnetic_limit");
+        claw_servo = hardwareMap.get(Servo.class, "claw");
 
-        //pumpkin_smasher = hardwareMap.get(DcMotor.class, "pumpkin_motor");
-        //candy_grabber = hardwareMap.get(Servo.class, "candy");
-
-        //pumpkin_smasher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-
-
-        fr_Wheel.setDirection(DcMotor.Direction.FORWARD);
-        fl_Wheel.setDirection(DcMotor.Direction.REVERSE);
-        br_Wheel.setDirection(DcMotor.Direction.FORWARD);
+        fr_Wheel.setDirection(DcMotor.Direction.REVERSE);
+        fl_Wheel.setDirection(DcMotor.Direction.FORWARD);
+        br_Wheel.setDirection(DcMotor.Direction.REVERSE);
         bl_Wheel.setDirection(DcMotor.Direction.REVERSE);
 
         fr_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fl_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        placing_slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        climbing_slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         telemetry.setMsTransmissionInterval(11);
         //limelight.pipelineSwitch(0);
         //limelight.start();
+        arm_servo.setPosition(0.64);
+        claw_servo.setPosition(1);
+
+
     }
+
+    double placing_Position = 0;
+    double previous_position = 0;
+    double previous_time = 0;
+
 
     public void loop() {
         //testing a change for github
@@ -65,81 +70,100 @@ public class MecanumTeleOpTesting extends OpMode {
         double bl_Position = bl_Wheel.getCurrentPosition();
         double fr_Position = fr_Wheel.getCurrentPosition();
         double br_Position = br_Wheel.getCurrentPosition();
+        double placing_Position = placing_slide.getCurrentPosition();
         double arm_Position = arm_servo.getPosition();
+        double claw_Position = claw_servo.getPosition();
 
         telemetry.addData("Front Left Wheel Pos", fl_Position);
         telemetry.addData("Back Left Wheel Pos", bl_Position);
         telemetry.addData("Front Right Wheel Pos", fr_Position);
         telemetry.addData("Back Right Wheel Pos", br_Position);
-
-        /*
-        LLResult result = limelight.getLatestResult();
-        if (result != null && result.isValid()) {
-            telemetry.addData("April Tag X Pos", result.getTx());
-            telemetry.addData("April Tag Y Pos", result.getTy());
-            telemetry.addData("April Tag Area %", result.getTa());
-            telemetry.addData("Limelight Pipeline", result.getPipelineIndex());
-        }
-        */
-
-        //pipeline 0 = chamber side
-        //pipeline 1 = basket side
-        //pipeline 2 = observation side
-
-        //double candy_bucket = candy_grabber.getPosition();
-        //telemetry.addData("Candy Bucket Pos", candy_bucket);
-
-        telemetry.update();
+        //telemetry.addData("Placing Slide Pos", placing_Position);
+        //telemetry.addData("Magnetic Limit Boolean", magnetic_limit.isPressed());
+        telemetry.addData("Arm Pos", arm_Position);
+        telemetry.addData("Claw Pos", claw_Position);
 
         // wheel movement
         double left_x = gamepad1.left_stick_x;
         double left_y = gamepad1.left_stick_y;
         double joystick_turn = gamepad1.right_stick_x;
-        double joystick_direction = Math.atan2(left_y, left_x);
+        double joystick_direction = -1 * Math.atan2(left_y, left_x);
         double joystick_magnitude = Math.sqrt((left_x * left_x) + (left_y * left_y));
 
-        // setting power of wheels based on joystick data
-        /*
-        fr_Wheel.setPower((-1 * Math.sin(joystick_direction + (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
-        fl_Wheel.setPower((Math.sin(joystick_direction + (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
-        br_Wheel.setPower((-1 * Math.sin(joystick_direction - (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
-        bl_Wheel.setPower((Math.sin(joystick_direction - (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
-         */
+        double left_x2 = gamepad2.left_stick_x / 2;
+        double left_y2 = gamepad2.left_stick_y / 2;
+        double joystick_turn2 = gamepad2.right_stick_x / 2;
+        double joystick_direction2 = -1 * Math.atan2(left_y2, left_x2) / 2;
+        double joystick_magnitude2 = Math.sqrt((left_x2 * left_x2) + (left_y2 * left_y2)) / 2;
 
-        fr_Wheel.setPower((Math.sin(joystick_direction + (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
-        fl_Wheel.setPower((-1 * Math.sin(joystick_direction - (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
-        br_Wheel.setPower((Math.sin(joystick_direction - (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
-        bl_Wheel.setPower((-1 * Math.sin(joystick_direction + (0.25 * Math.PI)) * joystick_magnitude + joystick_turn) / 2);
+        fr_Wheel.setPower((-1 * Math.sin((joystick_direction + joystick_direction2) - (0.25 * Math.PI)) * (joystick_magnitude + joystick_magnitude2) + (joystick_turn + joystick_turn2)) / 2);
+        br_Wheel.setPower((1 * Math.sin((joystick_direction + joystick_direction2) + (0.25 * Math.PI)) * (joystick_magnitude + joystick_magnitude2) - (joystick_turn + joystick_turn2)) / 2);
+        fl_Wheel.setPower((-1 * Math.sin((joystick_direction + joystick_direction2) + (0.25 * Math.PI)) * (joystick_magnitude + joystick_magnitude2) - (joystick_turn + joystick_turn2)) / 2);
+        bl_Wheel.setPower((1 * Math.sin((joystick_direction + joystick_direction2) - (0.25 * Math.PI)) * (joystick_magnitude + joystick_magnitude2) + (joystick_turn + joystick_turn2)) / 2);
 
         fr_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fl_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl_Wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        // slide movement
+        double right;
+        double left;
+        double right2;
+        double left2;
 
-        /*
-        // spoopy stuff
-        final double candy_racing = .01;
+        right = gamepad1.right_trigger;
+        left = gamepad1.left_trigger;
+        right2 = gamepad2.right_trigger;
+        left2 = gamepad2.left_trigger;
 
-        pumpkin_smasher.setPower((gamepad1.right_trigger - gamepad1.left_trigger) / 6);
-        if (gamepad1.x) {
-            //candy_bucket += candy_racing;
-            candy_grabber.setPosition(0.4);
-        } else if (gamepad1.y) {
-            //candy_bucket -= candy_racing;
-            candy_grabber.setPosition(0.6);
-        } else if (!gamepad1.x && !gamepad1.y) {
-            candy_grabber.setPosition(0.5);
+        //placing_slide.setPower(right + right2 - left - left2);
+        //telemetry.addData("Placing Slide Pos", placing_Position);
+
+        //linear slide P in PID?
+        double commanded_speed = right + right2 - left - left2;
+        if (commanded_speed > 1) {
+            commanded_speed = 1;
+        } else if (commanded_speed < -1) {
+            commanded_speed = -1;
         }
-        */
 
+        double current_position = placing_Position;
+        double current_time = runtime.seconds();
+
+        double change_in_position = current_position - previous_position;
+        double change_in_time = current_time - previous_time;
+
+        double actual_speed = change_in_position / change_in_time;
+        double speed_error = commanded_speed - actual_speed;
+        double error_constant = 0.3;
+
+        placing_slide.setPower(error_constant * speed_error);
+
+        previous_position = current_position;
+        previous_time = current_time;
+
+
+        // arm movement
+        final double arm_speed = 0.002;
+        if (gamepad2.dpad_down) {
+            arm_Position += arm_speed;
+        } else if (gamepad2.dpad_up) {
+            arm_Position -= arm_speed;
+        }
+        arm_servo.setPosition(arm_Position);
+
+        // claw movement
+        final double claw_speed = 0.003;
+        if (gamepad2.x) {
+            claw_Position += claw_speed;
+        } else if (gamepad2.a) {
+            claw_Position -= claw_speed;
+        }
+        claw_servo.setPosition(claw_Position);
+
+        telemetry.update();
     }
 }
 
-
-
-//bl_motor is actually br_motor
-//fl_motor is actually fr_motor
-//br_motor is actually fl_motor
-//fr_motor is actually bl_motor
 //bleh
